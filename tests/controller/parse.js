ffc427fef23d2ab9925d800f99f319f976a3fedf
Vincent Ting <homerdd@gmail.com>
2014-04-09 17:04:58 +0800

    Finish controller parse

--- tests/controller/parse.js
+++ tests/controller/parse.js
@@ -1,26 +1,18 @@
+var path = require('path');
 process.env['EGS_ROOT'] = path.join(process.cwd(), 'tests/apps');
 
 var should = require('should');
-var controller_parse = require('../../lib/core_loader/config_loader').parse;
+var controller_parse = require('../../lib/controller_parse').parse;
 
-describe('sys config', function () {
-    it('should from root', function (done) {
-        config_loader.sys['app_tasks_dir'].should.equal('tasks');
-        done();
-    });
-    it('should from app', function (done) {
-        config_loader.sys['redis_persist_key'].should.equal('queue');
-        done();
-    });
-    it('should be database', function (done) {
-        config_loader.database['mysql']['database'].should.equal('development');
-        done();
-    });
-});
 
-describe('app config', function () {
-    it('should routes', function (done) {
-        config_loader.load('demo', 'routes')['namespace'].should.equal('demo');
+describe('parse route_obj', function () {
+    it('should be obj', function (done) {
+        controller_parse('demo', "get /hello", "user#hello"
+        ).should.eql({
+                method: 'get',
+                url: '/hello',
+                callback: require('../apps/demo/controllers/user_controller').hello
+            });
         done();
     });
 });
