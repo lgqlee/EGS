8a510b7e36be762f0580f5465a14b48b738b0d3f
Vincent Ting <homerdd@gmail.com>
2014-04-17 19:01:43 +0800

    Fix a bug in model find

--- tests/activerecord/model.js
+++ tests/activerecord/model.js
@@ -1,26 +1,29 @@
 var path = require('path');
 var mysql = require('../../lib/active_record/db_pool').mysql;
+var redis = require('../../lib/active_record/db_pool').redis;
 process.env['EGS_ROOT'] = path.join(process.cwd(), 'tests/apps');
 
 var test_table = 'test_table';
 
 var db_init = function (fn) {
-    mysql.getConnection(function (err, connection) {
-        if (err) throw Error(err);
-        connection.query('CREATE DATABASE IF NOT EXISTS development', function (err) {
+    redis.flushdb(function () {
+        mysql.getConnection(function (err, connection) {
             if (err) throw Error(err);
-            connection.query('DROP TABLE IF EXISTS ' + test_table + ';', function (err) {
+            connection.query('CREATE DATABASE IF NOT EXISTS development', function (err) {
                 if (err) throw Error(err);
-                connection.query('create table ' + test_table + '  (id varchar(45) not null unique,name varchar(45), age varchar(45)); ', function (err) {
+                connection.query('DROP TABLE IF EXISTS ' + test_table + ';', function (err) {
                     if (err) throw Error(err);
-                    connection.query('insert into ' + test_table + ' values("123456", "vt", "15"),("123457", "paul", "22");', function (err) {
+                    connection.query('create table ' + test_table + '  (id varchar(45) not null unique,name varchar(45), age varchar(45)); ', function (err) {
                         if (err) throw Error(err);
-                        fn();
-                    })
+                        connection.query('insert into ' + test_table + ' values("123456", "vt", "15"),("123457", "paul", "22");', function (err) {
+                            if (err) throw Error(err);
+                            fn();
+                        })
+                    });
+                    connection.release();
                 });
-                connection.release();
-            });
-        })
+            })
+        });
     });
 };
 
@@ -79,19 +82,43 @@ describe('model create', function () {
     });
 });
 
-describe('model find', function () {
-
-});
-
-var User = Model.extend('b', {name: 'String:vt', age: 'Int:13'}, {db_name: test_table});
-
 describe('model where', function () {
+    var User = Model.extend('b', {name: 'String:vt', age: 'Int:13'}, {db_name: test_table});
     it('should length 1', function (done) {
         db_init(function () {
-            User.where({age:{gt:16}}, function(objs){
+            User.where({age: {gt: 16}}, function (objs) {
                 objs.length.should.equal(1);
+                objs[0].name.should.equal('paul');
                 done();
             });
+        });
+    });
+    it('should support limit', function (done) {
+        db_init(function () {
+            User.where({age: {gt: 0}}, function (objs) {
+                objs.length.should.equal(1);
+                objs[0].name.should.equal('vt');
+                done();
+            }, [0, 1]);
+        });
+    });
+});
+
+describe('model find', function () {
+    it('should return when redis not exist', function (done) {
+        var User = Model.extend('b', {name: 'String:vt', age: 'Int:13'}, {db_name: test_table, expire: 10});
+        db_init(function () {
+            User.find('123456', function (obj) {
+                obj.name.should.equal('vt');
+                done();
+            });
+        });
+    });
+    it('should return when redis exist', function (done) {
+        var User = Model.extend('b', {name: 'String:vt', age: 'Int:13'}, {db_name: test_table, expire: 10});
+        User.find('123456', function (obj) {
+            obj.name.should.equal('vt');
+            done();
         });
     });
 });
